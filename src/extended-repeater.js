b313eaad75a7971f35ab57cb0aa875c1fe13fa7c
Konstantin Lemko <lemko99@bk.ru>
2021-09-13 23:27:28 +0700

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,20 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let resultArray = [];
+  for(let i = 0; i < (options.repeatTimes || 1); i++) {
+    let string = String(str);
+    let addition = [];
+    for(let j = 0; j < (options.additionRepeatTimes || 1); j++) {
+      if(options.addition !== null) {
+        addition.push(options.addition)
+      } else {
+        addition.push(String(options.addition))
+      }
+    }
+    string += addition.join(options.additionSeparator || '|');
+    resultArray.push(string);
+  }
+  return resultArray.join(options.separator || '+');
 }
