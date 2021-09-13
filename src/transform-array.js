03366b3b62c9e52094ac642ce886e846de3a5a7d
Konstantin Lemko <lemko99@bk.ru>
2021-09-13 21:49:13 +0700

    feat: add solution for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,44 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if(!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  const array = [...arr];
+  for(let i = 0; i < array.length; i++) {
+    switch(array[i]) {
+      case('--discard-next'):
+        if(i < array.length - 1) {
+          array[i] = '';
+          array[i + 1] = '';
+        } else {
+          array.pop();
+        }
+        break;
+      case('--discard-prev'): 
+        if(i > 0) {
+          array[i] = '';
+          array[i - 1] = '';
+        } else {
+          array.shift();
+          i -= 1;
+        }
+        break;
+      case('--double-next'):
+        if(i < array.length - 1) {
+          array[i] = array[i + 1];
+        } else {
+          array.pop();
+        }
+        break;
+      case('--double-prev'): 
+        if(i > 0) {
+          array[i] = array[i - 1];
+        } else {
+          array.shift();
+          i -= 1;
+        }
+        break;
+    }
+  };
+  return array.filter(item => item !== '');
 }
