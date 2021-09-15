2921f9b333a63078a17912ff8f1e4d7a91858bbf
Konstantin Lemko <lemko99@bk.ru>
2021-09-15 10:40:05 +0700

    feat: add solution for vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,61 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.reverse = !bool;
+    this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if(message == undefined || key == undefined) throw new Error('Incorrect arguments!');
+    let encryptArray = [];
+    for(let i = 0, j = 0; i < message.length; i++) {
+      if(this.alphabet.includes(message[i].toUpperCase())) {
+        encryptArray[i] = key[j].toUpperCase();
+        j++;
+        if(j == key.length) {
+          j = 0;
+        }
+      }
+    }
+    let encriptedMessage = message.toUpperCase().split('').map((item, i) => {
+      if(this.alphabet.indexOf(item) != -1) {
+        let position = this.alphabet.indexOf(item);
+        let shiftPosition = this.alphabet.indexOf(encryptArray[i]);
+        let newPosition = (position + shiftPosition) % this.alphabet.length;
+        return this.alphabet[newPosition];
+      }
+      return item;
+    }).join('');
+    if(this.reverse) {
+      return encriptedMessage.split('').reverse().join('')
+    }
+    return encriptedMessage;
   }
+  decrypt(encryptedMessage, key) {
+    if(encryptedMessage == undefined || key == undefined) throw new Error('Incorrect arguments!');
+    let encryptArray = [];
+    for(let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if(this.alphabet.includes(encryptedMessage[i].toUpperCase())) {
+        encryptArray[i] = key[j].toUpperCase();
+        j++;
+        if(j == key.length) {
+          j = 0;
+        }
+      }
+    }
+    let message = encryptedMessage.toUpperCase().split('').map((item, i) => {
+      if(this.alphabet.indexOf(item) != -1) {
+        let position = this.alphabet.indexOf(item);
+        let shiftPosition = this.alphabet.indexOf(encryptArray[i]);
+        let newPosition = (position - shiftPosition + this.alphabet.length) % this.alphabet.length;
+        return this.alphabet[newPosition];
+      }
+      return item;
+    }).join('');
+    if(this.reverse) {
+      return message.split('').reverse().join('')
+    }
+    return message;
+  }
+  
 }
