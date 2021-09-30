12b5b94d350fab5a10caba2f162a235044cb3818
Konstantin Lemko <lemko99@bk.ru>
2021-09-30 12:44:59 +0300

    feat: add solution for st encode-line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,6 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  return str == '' ? '' : str.match(/((.)\2{0,})/g).map(item => item.length > 1 ? item.length + item[0] : item).join('');
 }
